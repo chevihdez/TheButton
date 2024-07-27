b621ffca1b2fbe2ba3b4a9679b2b3614d4895de0
chevi <chevihdez@gmail.com>
2024-07-26 17:41:15 -0700

    init commit

---
+++ Assets/Scripts/ToggleAnimationHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleAnimationHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Animator _animator;
+
+    public void SendAnimationTrigger(bool value)
+    {
+        _animator.SetTrigger(value.ToString());
+    }
+}
